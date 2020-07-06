2692ccff8aab75c222884b0b083d89a645f5b989
LiamMcFadden <liamm18@vt.edu>
2020-07-06 18:14:18 -0400

    updates sort of

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+
+window.addEventListener('scroll',  () => {
+    if (scrollY > 500) {
+        document.documentElement.style.setProperty
+    }
+});
